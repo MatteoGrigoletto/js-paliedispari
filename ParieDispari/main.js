56398361e0c2fccc0a2d1795339800e8f04f36e4
MatteoGrigoletto <mttgrigoletto@gmail.com>
2022-10-30 09:58:17 +0100

    annidamento condizioni

--- ParieDispari/main.js
+++ ParieDispari/main.js
@@ -2,27 +2,34 @@ let userValue = prompt(`scegli pari o dispari`);
 let userNumber = Number(prompt(`inserisci un numero da 1 a 5`));
 
 function calcolate(string,number){
-    let numberPc = Math.floor(Math.random() * 6);
-    let somma = number + numberPc;
-
-    if(somma % 2 === 0){
-        if(string === `pari`){
-            console.log(` il numero e' pari, hai vinto`)
-        }
-        else{
-             console.log(`il numero e' pari, hai perso`)
-        }
+    let somma;
+    
+    if(number > 5 || (string !== `pari` && string !== `dispari`)){
+        console.log('hai inserito un numero troppo grande, oppure una parola sbagliata')
     }
-    else if( somma % 2 === 1){
+    else{
+        let numberPc = Math.floor(Math.random() * 6);
+        somma = number + numberPc;
 
-        if(string === `dispari`){
-            console.log(`il numero e' dispari, hai vinto`);
+        if(somma % 2 === 0){
+            if(string === `pari`){
+                console.log(` il numero e' pari, hai vinto`)
+            }
+            else{
+                console.log(`il numero e' pari, hai perso`)
+            } 
         }
-        else{
-             console.log(`il numero e' dispari, hai perso`);
+        else if( somma % 2 === 1){
+            if(string === `dispari`){
+                console.log(`il numero e' dispari, hai vinto`);
+            }
+            else{
+                console.log(`il numero e' dispari, hai perso`);
+            } 
         }
+        return somma;
     }
-    return somma;
+    
 }
 
 console.log(calcolate(userValue, userNumber))
